1eb2d6339c5a107948dc0fd5608485e3400118ee
op07n <39182105+op07n@users.noreply.github.com>
2020-02-18 09:06:04 +0100

    Update streamlitWrapper.py

--- streamlitWrapper.py
+++ streamlitWrapper.py
@@ -4,4 +4,4 @@ import streamlit.cli
 import click
 click.pass_context
 if __name__ == '__main__':
-    streamlit.cli._main_run_clExplicit('pythonScript.py', 'streamlit run --global.developmentMode=false')
+    streamlit.cli._main_run_clExplicit('app.py', 'streamlit run --global.developmentMode=false')
